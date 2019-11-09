f0ff3668637c8f5f3e3fa836b3704a120c66f935
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2019-11-09 09:41:56 -0800

    added community post page

--- controllers/communitybuilds.js
+++ controllers/communitybuilds.js
@@ -1,16 +1,68 @@
 const express = require('express');
-const Community = require('../models/solar.js');
+const Community = require('../models/communitybuilds.js');
 const router = express.Router();
 
-
 router.get('/new', (req, res) => {
-    res.render('community/new.ejs');
+    res.render('community/new.ejs')
 });
-
 router.post('/', (req, res) => {
     Community.create(req.body, (error, createdCommunity) => {
-        res.redirect('community/new');
+        res.redirect('/community');
+
     });
+});
+
+router.get('/', (req, res) => {
+    if(req.session.username){
+        Community.find({}, (error, allCommunity) => {
+            res.render(
+                'community/index.ejs',
+                {
+                    community:allCommunity,
+                    username:req.session.username
+                }
+            );
+        })
+    } else {
+        res.redirect('/');
+    }
+});
+
+
+router.get('/:id/edit', (req, res) => {
+    Community.findById(req.params.id, (error, foundCommunity) => {
+        res.render('community/edit.ejs',{
+            community:foundCommunity
+        });
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    Community.findByIdAndRemove(req.params.id, (err, deletedCommunity) => {
+        res.redirect('/community');
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Community.findById(req.params.id, (error, foundCommunity) => {
+        res.render(
+            'community/show.ejs',
+            {
+                community:foundCommunity
+            }
+        );
+    });
+});
+
+router.put('/:id', (req, res) => {
+    Community.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (error, updatedModel) => {
+            res.redirect('/community');
+        }
+    )
 });
 
 
--- models/communitybuilds.js
+++ models/communitybuilds.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{type:String, required:true},
     image: String,
-    text: {type:String, required:true},
+    description: {type:String, required:true},
 });
 
 const Community = mongoose.model('Community', userSchema);
