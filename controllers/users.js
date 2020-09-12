a4d2aec86d685797a1886d2bb5057d1324a951d0
Veronica <vramirez@sandiego.edu>
2020-09-12 10:21:51 -1000

    updated git ignore

--- controllers/users.js
+++ controllers/users.js
@@ -6,29 +6,29 @@ const User = require('../models/users.js');
 
 router.get('/new', (req, res) => {
     res.render('users/new.ejs', {
-        usernameTaken:false
-    }); 
+        usernameTaken: false
+    });
 
 });
 
 //username already taken
 router.get("/new/usernametaken", (req, res) => {
     res.render("users/new.ejs", {
-        usernameTaken:true
+        usernameTaken: true
     });
 });
 
 router.post('/', (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     User.create(req.body, (error, createdUser) => {
-        if(createdUser){
-      req.session.username = createdUser.username;
-      req.session.userid = createdUser._id;
-      res.redirect('/solar')
-    } else {
-       res.redirect('/users/new/usernametaken')
-    }
-  })
+        if (createdUser) {
+            req.session.username = createdUser.username;
+            req.session.userid = createdUser._id;
+            res.redirect('/solar')
+        } else {
+            res.redirect('/users/new/usernametaken')
+        }
+    })
 })
 
 
